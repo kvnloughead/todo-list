5db26c380159c3aa54951abe4f12b833fc39bc1a
Kevin Loughead <kvnloughead@gmail.com>
2020-01-18 18:15:36 -0500

    Basic functionality complete.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,18 +1,30 @@
 
 checkOffTodo()
 trashTodo()
+newTodo()
 
 function checkOffTodo(){
-    $('.todo-item').on('click', function(){
+    $('ul').on('click', 'li', function(){
         $(this).toggleClass('done');
     })
 }
-
+ 
 function trashTodo(){
-    $(".trash").on('click', function(event){
+    $('ul').on('click', 'span', function(event){
         $(this).parent().fadeOut(function(){
             $(this).remove();
         });
+        event.stopPropagation();
     });
 }
 
+function newTodo(){
+    $('input[type="text"]').keypress(function(event){
+        if (event.which === 13){
+            var todo = $(this).val()
+            $(this).val("");
+            $('ul').append("<li><span>X</span> " + todo + "</li>");
+        };
+                 
+    })
+}
--- commit-changes.md
+++ commit-changes.md
@@ -3,3 +3,7 @@
 1. Project file structure in place.
 2. Basic html structure plus crossing out done tasks on click (`checkOffTodo: f`)
 3. Enable removal of todo items via trash can icon (`trashTodo: f`)
+4. Enabled creation of new todos
+5. Basic functionality complete.
+    - Added ability to add new todos
+    - Ensured that `checkOff` and `trash` listeners apply to all future todo items.
